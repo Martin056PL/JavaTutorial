b77eddd64e68d75e85bfff1c66626fcce74f249a
Kamil Wawer <Kamil.wawer92@gmail.com>
2018-11-05 00:11:30 +0100

    S01E08 - Hello world, print line,variables, updating variables

--- Main.java
+++ Main.java
@@ -1,7 +1,18 @@
 public class Main {
 
+
+
     public static void main(String[] args) {
 
+        int passengers;
+        passengers = 0;
+
         System.out.println("Hello World!");
+
+        passengers=passengers+5;
+        passengers=passengers-3;
+        passengers=passengers+-1+5;
+        System.out.println(passengers);
+
     }
 }
