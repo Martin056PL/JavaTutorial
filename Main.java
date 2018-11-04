ee91c596aa732806affef49eccf6246689189d21
Kamil Wawer <Kamil.wawer92@gmail.com>
2018-11-05 00:59:13 +0100

    S01E19 - Data and maths

--- Main.java
+++ Main.java
@@ -22,5 +22,22 @@ public class Main {
 
         System.out.println("The bus made $" + fare+ " after " + stops + " stops");
 
+        double div = 24/5.0;
+        System.out.println(div);
+
+        double current = 17;
+        double rate = 1.5;
+        double future = current*rate;
+        System.out.println(future);
+        int approx = (int) future;
+        System.out.println(approx);
+
+        int x = 5;
+        int y = 2;
+        double div2 = x/y;
+        System.out.println(x/y);
+        double accurate = (double) x/y;
+        System.out.println(accurate);
+
     }
 }
