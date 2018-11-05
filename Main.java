a37f23d5077036e8bc43ad937d0b2d680ce1b0a9
Kamil Wawer <Kamil.wawer92@gmail.com>
2018-11-06 00:26:46 +0100

    S2E10 - Else - if

--- Main.java
+++ Main.java
@@ -1,10 +1,16 @@
 public class Main {
 
-
-
     public static void main(String[] args) {
 
+        boolean isLightGreen = false;
+        boolean isLightYellow = false;
 
-
+        if(isLightGreen){
+            System.out.println("Drive!");
+        }else if (isLightYellow){
+            System.out.println("Slow down!");
+        }else{
+            System.out.println("Stop!");
+        }
     }
 }
