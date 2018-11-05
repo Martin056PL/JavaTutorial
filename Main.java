8d55d0cba06900e87d85769553a55fb0a1b041de
Kamil Wawer <Kamil.wawer92@gmail.com>
2018-11-06 00:39:49 +0100

    S2E16 - Museum Code

--- Main.java
+++ Main.java
@@ -2,15 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        boolean isLightGreen = false;
-        boolean isLightYellow = false;
+        boolean isStudent = true;
+        int ticketPrice = 10;
+        int age = 30;
 
-        if(isLightGreen){
-            System.out.println("Drive!");
-        }else if (isLightYellow){
-            System.out.println("Slow down!");
+        if((age<=15||age>60)||isStudent){
+            System.out.println("Discount! 5$ for ticket");
         }else{
-            System.out.println("Stop!");
+            System.out.println("Normal ticket");
         }
     }
 }
