065f18f0ca45fd5f3b7bafb38adda867904be8c0
Kamil Wawer <Kamil.wawer92@gmail.com>
2018-11-06 23:46:42 +0100

    S4E08 - Random numbers - Dice - Yahtzee

--- Main.java
+++ Main.java
@@ -1,27 +1,43 @@
-import java.util.Scanner;
-
 public class Main {
+
+
 
     public static void main(String[] args) {
 
-    rollDice();
-    rollDice();
-
+        abc();
     }
 
+    public static int rollDice(){
 
-    public static int rollDice (){
+        double dNumber = Math.random();
+        dNumber = dNumber*6;
+        dNumber = dNumber+1;
+        int intNumber = (int) dNumber;
+        return intNumber;
+    }
 
-        double randomNumber = Math.random();
-        randomNumber = randomNumber*6;
-        randomNumber = randomNumber+1;
-        int randomInt = (int) randomNumber;
-        System.out.println("Roll: "+ randomInt);
-        return randomInt;
-
-
-      }
-
+    public static int abc(){
+        int dice1 = rollDice();
+        int dice2 = rollDice();
+        int dice3 = rollDice();
+        int dice4 = rollDice();
+        int dice5 = rollDice();
+        int dice6 = rollDice();
+        int dice7 = rollDice();
+        int i = 1;
+        while(!(dice1==dice2&&dice2==dice3&&dice3==dice4&&dice4==dice5&&dice5==dice6&&dice6==dice7)){
+            dice1 = rollDice();
+            dice2 = rollDice();
+            dice3 = rollDice();
+            dice4 = rollDice();
+            dice5 = rollDice();
+            dice6 = rollDice();
+            dice7 = rollDice();
+            i=i+1;
+        }
+        System.out.println(i);
+        return i;
+    }
 }
 
 
