23b63db4a69dc588d7aed8e097edaa4624f8297b
Kamil Wawer <Kamil.wawer92@gmail.com>
2018-11-07 12:23:52 +0100

    S4E12 - Count the blocks

--- Main.java
+++ Main.java
@@ -3,41 +3,23 @@ public class Main {
 
 
     public static void main(String[] args) {
+    countBlocks(10);
 
-        abc();
     }
 
-    public static int rollDice(){
+    public static int countBlocks(int levels){
 
-        double dNumber = Math.random();
-        dNumber = dNumber*6;
-        dNumber = dNumber+1;
-        int intNumber = (int) dNumber;
-        return intNumber;
-    }
+        int totalBlocks = 0;
 
-    public static int abc(){
-        int dice1 = rollDice();
-        int dice2 = rollDice();
-        int dice3 = rollDice();
-        int dice4 = rollDice();
-        int dice5 = rollDice();
-        int dice6 = rollDice();
-        int dice7 = rollDice();
-        int i = 1;
-        while(!(dice1==dice2&&dice2==dice3&&dice3==dice4&&dice4==dice5&&dice5==dice6&&dice6==dice7)){
-            dice1 = rollDice();
-            dice2 = rollDice();
-            dice3 = rollDice();
-            dice4 = rollDice();
-            dice5 = rollDice();
-            dice6 = rollDice();
-            dice7 = rollDice();
-            i=i+1;
+        for (int i=1; i<=levels ; i++){
+            int blocks = i*i;
+            totalBlocks = totalBlocks+blocks;
+            System.out.println("Total blocks: " + totalBlocks);
         }
-        System.out.println(i);
-        return i;
+
+        return 0;
     }
+
 }
 
 
