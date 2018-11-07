62a1ed1d762adeb3804652b3d466515bb0126626
Kamil Wawer <Kamil.wawer92@gmail.com>
2018-11-07 13:32:16 +0100

    S4E18 - Casino Symulator

--- Main.java
+++ Main.java
@@ -1,23 +1,51 @@
 public class Main {
 
 
-
     public static void main(String[] args) {
-    countBlocks(10);
-
+        martingale();
     }
 
-    public static int countBlocks(int levels){
 
-        int totalBlocks = 0;
+    public static int martingale() {
+        int money = 1000;
+        int target = 1200;
+        int bet = 10;
+        while (money > bet) {
+            boolean win = play();
+            if (win) {
+                money += bet;
+                bet = 10;
+                System.out.println("Wygrałeś");
+                if (money >= target) {
+                    System.out.println("Jackpot!");
+                    break;
+                }
+            } else {
+                money -= bet;
+                bet *= 2;
+                System.out.println("Przegrałeś!");
+                if (money < bet) {
+                    System.out.println("Bankrut");
+                    break;
+                }
+            }
 
-        for (int i=1; i<=levels ; i++){
-            int blocks = i*i;
-            totalBlocks = totalBlocks+blocks;
-            System.out.println("Total blocks: " + totalBlocks);
         }
+        return money;
+    }
 
-        return 0;
+    public static boolean play(){
+        boolean win;
+        double doubleNumber = Math.random();
+        doubleNumber = doubleNumber*2;
+        doubleNumber = doubleNumber+1;
+        int intNumber = (int) doubleNumber;
+        if (intNumber==1){
+            win = true;
+        }else{
+            win = false;
+        }
+        return win;
     }
 
 }
